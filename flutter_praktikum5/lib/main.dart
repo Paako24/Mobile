7cc506eddc8138ea5aa6c67be0f9d5173c2471fc
Bagus Setiyo <bagussp40@gmail.com>
2023-03-30 04:24:49 +0700

    praktikum 5

---
+++ flutter_praktikum5/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_5/article.dart';
+import 'package:flutter_application_5/detail_page.dart';
+import 'package:flutter_application_5/list_page.dart';
+import 'package:flutter_application_5/web_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: NewsListPage.routeName,
+      routes: {
+        NewsListPage.routeName: (context) => NewsListPage(),
+        ArticleDetailPage.routeName: (context) => ArticleDetailPage(
+            article: ModalRoute.of(context)?.settings.arguments as Article,
+        ),
+        ArticleWebView.routeName: (context) => ArticleWebView(
+            url: ModalRoute.of(context)?.settings.arguments as String,
+        ),
+      },
+    );
+  }
+}
